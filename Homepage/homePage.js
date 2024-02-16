d854fffe0e1ccb17987d576572f778b21fe9edd1
amirmacic <133912285+amirmacic@users.noreply.github.com>
2024-02-16 08:40:43 +0100

    [categorize/directories]

---
+++ Homepage/homePage.js
@@ -0,0 +1,13 @@
+const showObjaviOglasOnClick = () => {
+    document.getElementById("modal").style.display = "block";
+}
+
+const hideObjaviOglas = () => {
+    document.getElementById("modal").style.display = "none";
+}
+
+const ucitajFooter = () => {
+document.addEventListener('DOMContentLoaded', async () => {
+    await ucitajFooter();
+}, false); 
+}
