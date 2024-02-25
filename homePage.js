e9fd7a17b501b91070e69656fd59837bf8614a1d
amirmacic <133912285+amirmacic@users.noreply.github.com>
2024-02-25 09:59:05 +0100

    [zadnje/izmjene]

--- homePage.js
+++ homePage.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 const showObjaviOglasOnClick = () => {
     document.getElementById("modal").style.display = "block";
 }
@@ -13,33 +11,3 @@ document.addEventListener('DOMContentLoaded', async () => {
     await ucitajFooter();
 }, false); 
 }
-=======
-const showObjaviOglasOnClick = () => {
-    document.getElementById("modal").style.display = "block";
-}
-
-const hideObjaviOglas = () => {
-    document.getElementById("modal").style.display = "none";
-}
-
-const ucitajFooter = () => {
-document.addEventListener('DOMContentLoaded', async () => {
-    await ucitajFooter();
-}, false); 
-}
->>>>>>> dbea0f7678f57cd5a0b67ec3f7acf3593bd6c7b3
-=======
-const showObjaviOglasOnClick = () => {
-    document.getElementById("modal").style.display = "block";
-}
-
-const hideObjaviOglas = () => {
-    document.getElementById("modal").style.display = "none";
-}
-
-const ucitajFooter = () => {
-document.addEventListener('DOMContentLoaded', async () => {
-    await ucitajFooter();
-}, false); 
-}
->>>>>>> dbea0f7678f57cd5a0b67ec3f7acf3593bd6c7b3
